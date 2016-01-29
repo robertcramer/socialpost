80b48d7abeed1acd66454861f3e4e6c12ed0dcfc
robertcramer <robertbcramer@icloud.com>
2016-01-29 14:20:09 -0500

    fixed javascript directory

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,3 +1,4 @@
 //= require angular
+//= require angular-rails-templates
 //= require angular-ui-route
 //= require_tree
---
+++ app/assets/javascript/posts/postsCtrl.js
@@ -0,0 +1,30 @@
+angular.module('flapperNews')
+.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+
+        $scope.post = posts.posts[$stateParams.id];
+
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0,
+            comments: [
+                {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            ]
+        });
+
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+
+    }]);
